a048510b14df09cfd4c0fc20fe1e22e3ed86fa81
Liam <lharvey7@cps.edu>
2023-12-11 10:30:24 -0600

    finished part4 code

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -9,19 +9,37 @@ data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
 x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
-# Step 1: Print the values for x and y
 
-# Step 2: Standardize the data using StandardScaler, 
+print(x)
+print(y)
 
-# Step 3: Transform the data
+scaler = StandardScaler().fit(x)
 
-# Step 4: Split the data into training and testing data
+x = scaler.transform(x)
 
-# Step 5: Fit the data
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
-# Step 6: Create a LogsiticRegression object and fit the data
 
-# Step 7: Print the score to see the accuracy of the model
+model = linear_model.LogisticRegression().fit(x_train, y_train)
 
-# Step 8: Print out the actual ytest values and predicted y values
-# based on the xtest data
+print(f"Accuracy of Model: {model.score(x_test, y_test)}")
+
+print(y_test)
+
+for index in range(len(x_test)):
+    x = x_test[index]
+    x = x.reshape(-1, 3)
+    y_pred = int(model.predict(x))
+
+    if y_pred == 0:
+        y_pred = "Male"
+    elif y_pred == 1:
+        y_pred = "Female"
+
+    actual = y_test[index]
+    if actual == 0:
+        actual = "Male"
+    elif actual == 1:
+        actual = "Female"
+    print("Predicted Gender: " + y_pred + " Actual Gender: " + actual)
+    print("")
